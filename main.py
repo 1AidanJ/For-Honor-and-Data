2149c27c24f78e3a07491255b8df3ebd565bf3ce
1AidanJ <51326009+1AidanJ@users.noreply.github.com>
2023-04-07 16:38:44 -0400

    war crimes are fun times
    basic window setup. Nothing more to say.

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-# suffering
-# so many deadlines, so little time
-# it's not even my fault. Things would be so much easier if I didnt have two group projects I had to do solo.
+import tkinter
+import sqlite3
+import matplotlib
+import pandas
+from tkinter import *
+from tkinter import ttk
+
+win = tkinter.Tk()
+win.geometry("800x400")
+win.title("For Honor and Data - Main")
+main = Menu(win)
+
+win.config(menu=main)
+win.mainloop()
